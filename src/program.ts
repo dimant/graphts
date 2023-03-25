2f4a49305cfdae36d567c2f16171f260c7501274
Diman Todorov <diman.todorov@outlook.com>
2023-03-25 15:06:24 -0700

    stuff

--- src/program.ts
+++ src/program.ts
@@ -3,23 +3,22 @@ import { Application } from './Application';
 import { ServicePrincipal } from './ServicePrincipal';
 import { RestClient } from './restclient';
 
-async function listApplications(accessToken: string) {
-  const client = new RestClient('https://graph.microsoft.com/v1.0', accessToken);
-  const applications = await client.get<Application[]>('/applications');
-  return applications;
-}
-
-async function listServicePrincipals(accessToken: string) {
-  const client = new RestClient('https://graph.microsoft.com/v1.0', accessToken);
-  const servicePrincipals = await client.get<ServicePrincipal[]>('/servicePrincipals');
-  return servicePrincipals;
-}
-
 async function main() {
+  const endpoint = 'https://graph.microsoft.com/v1.0';
   const token = await az.azGetMsGraphToken();
-  const result = await listServicePrincipals(token.accessToken);
+  const client = new RestClient(endpoint, token.accessToken);
+  const applications = await client.get<Application[]>('/applications');
+  const servicePrincipals = await client.get<ServicePrincipal[]>('/servicePrincipals');
 
-  console.log(result);
+  console.log("Applications:");
+  applications.forEach((application) => {
+    console.log(`  ${application.displayName} (${application.id})`);
+  });
+
+  console.log("Service Principals:");
+  servicePrincipals.forEach((servicePrincipal) => {
+    console.log(`  ${servicePrincipal.displayName} (${servicePrincipal.id})`);
+  });
 }
 
 main().then(() => {
