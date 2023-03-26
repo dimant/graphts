96c1c2813c79beb56ce92658cbc0adedec654e74
Diman Todorov <diman.todorov@outlook.com>
2023-03-26 06:28:53 -0700

    stuff

---
+++ src/credlogin.ts
@@ -0,0 +1,35 @@
+import * as OTPAuth from "otpauth";
+import axios from 'axios';
+
+interface ICredential {
+    tenantId: string;
+    clientId: string;
+    username: string;
+    password: string;
+    totp_secret: string;
+    scope: string;
+}
+
+export async function getMsGraphToken(credential:ICredential) : Promise<string>
+{
+    const totp = new OTPAuth.TOTP({
+      digits: 6,
+      period: 30,
+      secret: credential.totp_secret
+    });
+    const totp_code = totp.generate();
+  
+    const authUrl = `https://login.microsoftonline.com/${credential.tenantId}/oauth2/v2.0/token`;
+    const authHeaders = { 'Content-Type': 'application/x-www-form-urlencoded' };
+    const authData = new URLSearchParams({
+      grant_type: 'password',
+      scope: credential.scope,
+      client_id: credential.clientId,
+      username: credential.username,
+      password: credential.password,
+      totp: totp_code
+    });
+    const authResponse = await axios.post(authUrl, authData.toString(), { headers: authHeaders });
+
+    return authResponse.data.access_token;
+}
--- src/program.ts
+++ src/program.ts
@@ -1,12 +1,14 @@
 import * as az from './azlogin'
+import * as login from './credlogin'
+import * as cred from './user_credentials.cred'
+
 import { Application } from './Model/Application';
 import { ServicePrincipal } from './Model/ServicePrincipal';
 import { RestClient } from './RestClient';
 
-async function main() {
+async function graph(accessToken: string) {
   const endpoint = 'https://graph.microsoft.com/beta';
-  const token = await az.azGetMsGraphToken();
-  const client = new RestClient(endpoint, token.accessToken);
+  const client = new RestClient(endpoint, accessToken);
   const applications = await client.get<Application[]>('/applications');
   const servicePrincipals = await client.get<ServicePrincipal[]>('/servicePrincipals');
 
@@ -19,6 +21,12 @@ async function main() {
   servicePrincipals.forEach((servicePrincipal) => {
     console.log(`  ${servicePrincipal.displayName} (${servicePrincipal.id})`);
   });
+}
+
+async function main() {
+  const accessToken = await login.getMsGraphToken(cred.test_user_1_cred());
+
+  await graph(accessToken);
 }
 
 main().then(() => {
