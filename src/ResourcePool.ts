31da3e65553932e6cc391b9e30d908553727752b
Diman Todorov <diman.todorov@outlook.com>
2023-03-26 19:04:46 -0700

    some doc

--- src/ResourcePool.ts
+++ src/ResourcePool.ts
@@ -1,11 +1,17 @@
+/// Manages a pool of resources, allowing them to be acquired and released.
+/// If no resources are available, the acquire method will wait until one is released.
+/// If a resource is released when there are no waiters, it will be cleaned up
+/// using the cleanup callback and then returned to the pool.
 export class ResourcePool<T>
 {
     private readonly resources: T[] = [];
     private readonly waiters: ((resource: T) => void)[] = [];
+    private readonly cleanup: (resource:T) => void;
 
-    constructor(initialResources: T[])
+    constructor(initialResources: T[], cleanup: (resource:T) => void)
     {
         this.resources = initialResources;
+        this.cleanup = cleanup;
     }
 
     public async acquire(): Promise<T>
@@ -21,6 +27,7 @@ export class ResourcePool<T>
 
     public release(resource: T)
     {
+        this.cleanup(resource);
         this.resources.push(resource);
 
         if (this.waiters.length > 0) {
